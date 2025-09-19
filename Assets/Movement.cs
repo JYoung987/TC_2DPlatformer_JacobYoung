53e5f2706be0e2e7f4a6a9919e2409f37f410154
JYoung987 <Y3036816@southstaffs.ac.uk>
2025-09-19 16:04:42 +0100

    Added animations

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,30 +4,48 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
     public float m_Speed = 10f;
     public float m_JumpPower = 10f;
-    Rigidbody2D m_Rigidbody;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
 
-    // Start is called before the first frame update
+    private Animator anim;
+    private Rigidbody2D m_Rigidbody;
+    private bool isGrounded;
+
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         float xMove = Input.GetAxisRaw("Horizontal");
         float yMove = Input.GetAxisRaw("Vertical");
+        anim.SetBool("run", xMove != 0);
 
         m_Rigidbody.linearVelocity = new Vector2(xMove * m_Speed, m_Rigidbody.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        // Flip character based on direction
+        if (xMove > 0)
         {
-            m_Rigidbody.AddForce(Vector2.up * m_JumpPower, ForceMode2D.Impulse);
-        
+            transform.localScale = new Vector3(3, 3, 3);
+        }
+        else if (xMove < 0)
+        {
+            transform.localScale = new Vector3(-3, 3, 3);
         }
 
+        // Check if grounded
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            m_Rigidbody.AddForce(Vector2.up * m_JumpPower, ForceMode2D.Impulse);
+            anim.SetTrigger("jump");
+        }
     }
 }
+

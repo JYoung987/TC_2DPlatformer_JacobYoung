2e8a51db350625ef6b7df2d101b29dcf80dba742
JYoung987 <Y3036816@southstaffs.ac.uk>
2025-09-25 11:50:22 +0100

    Background fixed and title screen implemented

--- Assets/Cloud_spawn_script.cs
+++ Assets/Cloud_spawn_script.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Cloud_spawn_script : MonoBehaviour
 {
     public GameObject Cloud;
+    public float spawnRate = 2;
+    private float timer = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +15,16 @@ public class Cloud_spawn_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Instantiate(Cloud, transform.position, transform.rotation);
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(Cloud, transform.position, transform.rotation);
+            timer = 0;
+        }
+        
         
     }
 }
